17a07c46ac0deca9bfc47873997a5c29a522e6a2
FrancoConci <franco1conci@gmail.com>
2024-04-06 11:56:35 +0200

    Get server port from env.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 
-const port = 3000;
-
 express()
   .use(cors())
   .get('/', (_, res) => {
     res.send(JSON.stringify({ SOMETHING: 'ELSE' }));
   })
-  .listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  .listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`);
   });
